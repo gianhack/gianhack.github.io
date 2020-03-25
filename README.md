4a8f5d8e1603582ed26c90569d8194de09ae955e
Giancarlo Rocha <giancarloiff@gmail.com>
2020-03-25 09:51:08 -0300

    clean working directory

--- README.md
+++
@@ -1,2 +0,0 @@
-# giancarlopro.github.io
-
--- palindrome/palindrome.js
+++
@@ -1,8 +0,0 @@
-function is_palindrome(word){
-    return word == word.split('').reverse().join('');
-}
-if (is_palindrome(prompt('Palavra:'))){
-    alert("É palindrome");
-} else {
-    alert("Não é palindrome");
-}
